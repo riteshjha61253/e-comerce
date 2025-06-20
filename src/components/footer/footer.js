c6f60b184fef4eb8d039be92b3b59908fd355584
riteshjha61253 <jha61253@gmail.com>
2025-06-20 20:34:18 +0530

    Added header and footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../footer/footer.css';
+
+const Footer = () => {
+    return(
+        <footer>
+            <p>© 2023 E-commerce Store. All rights reserved.</p>
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/header/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import '../header/header.css';
+
+const Header = () => {
+    return(
+        <header>
+            <h1>Welcome to Our E-commerce Store</h1>
+        </header>
+    )
+}
+
+export default Header;
